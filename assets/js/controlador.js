f9b641081bbb38f678273d388f47b37a65bd622c
Angelly <angellyjessibelelvir@gmail.com>
2023-07-30 12:48:49 -0600

    ventanas conectadas

---
+++ assets/js/controlador.js
@@ -0,0 +1,24 @@
+function irEspecificaciones(){
+    window.open('http://127.0.0.1:5500/especificaciones.html')
+}
+function irCarrito(){
+    window.open('http://127.0.0.1:5500/carrito.html')
+}
+function irCelulares(){
+    window.open('http://127.0.0.1:5500/celulares.html')
+}
+function irIniciarSesion(){
+    window.open('http://127.0.0.1:5500/iniciar-sesion.html')
+}
+function irCrearCuenta(){
+    window.open('http://127.0.0.1:5500/crear-cuenta.html')
+}
+function irEnvio(){
+    window.open('http://127.0.0.1:5500/envio.html')
+}
+function irModoDePago(){
+    window.open('http://127.0.0.1:5500/modo-de-pago.html')
+}
+function irResumenCompra(){
+    window.open('http://127.0.0.1:5500/resumen-compra.html')
+}
